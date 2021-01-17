3fc6e604f247038c23736f8bf6ad6489ec61f069
ahmedhabeeb73 <alnaib888@gmail.com>
2021-01-17 12:06:28 +0100

    setup auth ,nav,migration

---
+++ database/migrations/2021_01_17_104505_create_works_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('works', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('title');
+            $table->string('location');
+            $table->string('description');
+            $table->text('content');
+            $table->string('image');
+            $table->string('link');
+            $table->string('contact');
+            $table->timestamp('published_at')->nullable();
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works');
+    }
+}
