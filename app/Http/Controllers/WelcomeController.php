61158c2cff303368800dada1c04a025a208fd3dc
ahmedhabeeb73 <alnaib888@gmail.com>
2021-01-21 19:36:30 +0100

    search function

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,10 +9,19 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $search = $request->query('search');
+      if($search) {
+          $works=Work::where('title', 'LIKE', "%{$search}%")
+                    ->orWhere('name', 'LIKE', "%{$search}%")
+              ->simplePaginate(1);
+      } else{
+          $works=Work::latest()->simplePaginate(8);
+      }
+
         return view('welcome')
-               ->with('works',Work::latest()->simplePaginate(8))
+               ->with('works',$works)
                ->with('categories',Category::all())
                ->with('tags',Tag::all());
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-@include('layouts.header')
+
+    <div class="p-4 p-md-5 mb-4 text-white min-vh-100 d-flex align-items-center rounded bg-showcase">
+        <div class="col-md-8 px-0">
+            <h1 class="display-4 font-italic">Ready to get something awesome?</h1>
+            <p class="lead my-3">Practical screencasts for awesome developers..</p>
+            <form action="{{route('welcome.index')}}" method="GET">
+                <div class="input-group input-group-lg mb-3">
+                    <input name="search" type="text" value="{{request()->query('search')}}" class="form-control py-4" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" placeholder="What would you like to search? e.g. Helfer, Amazon">
+                </div>
+            </form>
+            <p class="lead mb-0">Or Just <a href="{{route('jobs')}}" class="text-blue-500 fw-bold">Browse the jobs</a></p>
+        </div>
+    </div>
+
+
     <div class="container pt-5">
     <div class="row">
         <div class="col-md-8 ">
-            @foreach($works as $work)
+            @forelse($works as $work)
             <div class=" bg-white mb-4 row align-items-center p-2">
               <div class="col-sm">
                   <img src="{{asset($work->image)}}" width="60px" height="60px" style="border-radius: 50%" alt="">
@@ -22,7 +36,11 @@
                 </div>
 
             </div>
-            @endforeach
+            @empty
+                <p class="text-center">
+                    No results found <strong>{{request()->query('search')}}</strong>
+                </p>
+            @endforelse
         </div>
 
         <div class="col-md-4">
@@ -38,7 +56,7 @@
 
         </div>
     </div>
-        {{ $works->links() }}
+        {{ $works->appends(['search'=>request()->query('search')])->links() }}
     </div>
 
 @endsection
